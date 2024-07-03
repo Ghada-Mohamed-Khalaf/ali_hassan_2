d004884318a7f10765e695234fa8a2916eb957e8
Ghada-Mohamed-Khalaf <gmo625112@gmail.com>
2024-07-03 11:40:32 +0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/level_1_4_1_ali/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.level_1_4_1_ali
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: FacebookApp() ,
+    );
+  }
+}
+class FacebookApp extends StatelessWidget {
+  const FacebookApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.teal,
+      appBar: AppBar(
+        elevation: 20,
+        leading: Icon(Icons.menu),
+        centerTitle: true,
+        title: Text("facebook"),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: Icon(
+              Icons.message,
+              size: 22,
+            ),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: Icon(
+              Icons.search,
+              size: 22,
+            ),
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(onPressed: (){},child:Icon(Icons.add) ,),
+      body: Text("The Text widget displays a string of text with single style. The string might break across multiple lines or might all be displayed on the same line depending on the layout constraints. ",style: TextStyle(),maxLines: 2,overflow: TextOverflow.fade,),
+
+    );
+  }
+}
+
